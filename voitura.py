49d38b79a5aff31984108cc7dfb5ff02eea73b52
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2020-11-08 15:23:46 +0100

    tout zelda

--- voitura.py
+++ voitura.py
@@ -5,7 +5,7 @@ root = Tk()
 frame = Frame(root, width=1100, height=800)
 frame.pack(expand=True, fill=BOTH)
 
-canvas = Canvas(frame, bg="white", width=1000, height=700)
+canvas = Canvas(frame, bg="white", width=1500, height=700)
 
 coordinates = 600, 400, 700, 500
 mur_jaune = canvas.create_rectangle(coordinates, fill="yellow")
@@ -16,10 +16,25 @@ voiture = canvas.create_image(150, 150, image=photo)
 
 
 def roule(event):
-    canvas.move(voiture, 1, 0)
+    canvas.move(voiture, 10, 0)
+
+
+def recule(event):
+    canvas.move(voiture, -10, 0)
+
+
+def monte(event):
+    canvas.move(voiture, 0, -10)
+
+
+def descend(event):
+    canvas.move(voiture, 0, 10)
 
 
 frame.focus_set()
 frame.bind("<Right>", roule)
+frame.bind("<Left>", recule)
+frame.bind("<Up>", monte)
+frame.bind("<Down>", descend)
 
 root.mainloop()
