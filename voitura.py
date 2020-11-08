06a3d738c03f7a0784a6a1275ca8a4f775a6f42e
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2020-11-08 14:52:21 +0100

    Premier code de zelda !!

--- voitura.py
+++ voitura.py
@@ -8,8 +8,18 @@ frame.pack(expand=True, fill=BOTH)
 canvas = Canvas(frame, bg="white", width=1000, height=700)
 
 coordinates = 600, 400, 700, 500
-arc = canvas.create_rectangle(coordinates, fill="yellow")
+mur_jaune = canvas.create_rectangle(coordinates, fill="yellow")
 
 canvas.pack(expand=True, fill=BOTH)
+photo = PhotoImage(file="voiture1.png")
+voiture = canvas.create_image(150, 150, image=photo)
+
+
+def roule(event):
+    canvas.move(voiture, 1, 0)
+
+
+frame.focus_set()
+frame.bind("<Right>", roule)
 
 root.mainloop()
