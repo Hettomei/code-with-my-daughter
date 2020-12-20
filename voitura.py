6d71c24c6b50515882d740c7f53fb34814c8247f
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2020-12-20 10:00:40 +0100

    add readme

--- voitura.py
+++ voitura.py
@@ -7,12 +7,15 @@ frame.pack(expand=True, fill=BOTH)
 
 canvas = Canvas(frame, bg="white", width=1500, height=700)
 
-coordinates = 600, 400, 700, 500
-mur_jaune = canvas.create_rectangle(coordinates, fill="yellow")
+coordinates = 600, 400, 800, 500
 
 canvas.pack(expand=True, fill=BOTH)
 photo = PhotoImage(file="voiture1.png")
 voiture = canvas.create_image(150, 150, image=photo)
+mur_jaune = canvas.create_rectangle(coordinates, fill="yellow")
+lbl = Label(canvas, text="garage", bg="yellow", font=("Courier", 30)).place(
+    x=610, y=410
+)
 
 
 def roule(event):
