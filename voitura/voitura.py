e118e63e6db761f7cceb31342092358a8c696ceb
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2022-03-02 21:14:01 +0100

    Add a moving car

--- voitura/voitura.py
+++ voitura/voitura.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import sys
+from turtle import color
 import pygame
 
 
@@ -20,6 +21,10 @@ def dessine_route_vertical():
 
     canvas.create_line(1200, 0, 1200, 1200, width=5)
 
+def dessine_stop(x,y):
+    canvas.create_line(x, y, x, y+100, width=8,fill="purple")
+    canvas.create_oval(x-30,y-30,x+30,y+30,fill="red")
+    Label(canvas, text="STOP", bg="red",fg="white" , font=("Courier", 10) ).place(x=x-15, y=y)
 
 def fait_pouet(event):
     pygame.mixer.Sound.play(klaxon_sound)
@@ -28,7 +33,6 @@ def fait_pouet(event):
 def fait_pouet_monte(event):
     pygame.mixer.Sound.play(klaxon_sound)
     canvas.move(voiture_bloque, 0, -10)
-
 
 def fait_pouet_descend(event):
     pygame.mixer.Sound.play(klaxon_sound)
@@ -44,6 +48,12 @@ def fait_pouet_avance(event):
     pygame.mixer.Sound.play(klaxon_sound)
     canvas.move(voiture_bloque, 10, 0)
 
+def automatique_descend():
+    canvas.move(voiture_roule, 0, 2)
+    x,y, *_ = canvas.bbox(voiture_roule)
+    if y > 1100:
+        canvas.coords(voiture_roule, 1150, 20)
+    canvas.after(20, automatique_descend)
 
 def roule(event):
     canvas.move(voiture, 10, 0)
@@ -80,10 +90,14 @@ coordinates = 600, 400, 800, 500
 canvas.pack(expand=True, fill=BOTH)
 photo = PhotoImage(file="voiture1.png")
 photo_bloque = PhotoImage(file="voiture3.png")
+photo_roule = PhotoImage(file="voiture5.png")
 dessine_route_horizontale()
 dessine_route_vertical()
+dessine_stop(1000,320)
 voiture = canvas.create_image(-10, 440, image=photo)
 voiture_bloque = canvas.create_image(430, 440, image=photo_bloque)
+voiture_roule = canvas.create_image(1150, 20, image=photo_roule)
+
 mur_jaune = canvas.create_rectangle(coordinates, fill="yellow")
 Label(canvas, text="garage", bg="yellow", font=("Courier", 30)).place(x=610, y=410)
 
@@ -99,6 +113,8 @@ frame.bind("<KP_8>", fait_pouet_monte)
 frame.bind("<KP_5>", fait_pouet_descend)
 frame.bind("<KP_4>", fait_pouet_recule)
 frame.bind("<KP_6>", fait_pouet_avance)
+
+automatique_descend()
 
 
 root.mainloop()
